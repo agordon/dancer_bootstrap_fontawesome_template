d5438d3de25aa971cd7b9375f63b897a59ce4dda
A. Gordon <gordon@cshl.edu>
2012-05-25 12:16:48 -0400

    Deployment wizard: added apache+CGI stub.

--- lib/dancer_bootstrap_fontawesome_template.pm
+++ lib/dancer_bootstrap_fontawesome_template.pm
@@ -1,10 +1,23 @@
 package dancer_bootstrap_fontawesome_template;
 use Dancer ':syntax';
+use strict;
+use warnings;
+use Cwd;
+use Sys::Hostname;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+get '/deploy' => sub {
+    my $project_dir = getcwd();
+    my $hostname = hostname;
+    template 'deployment_wizard', {
+		directory => $project_dir,
+		hostname  => $hostname
+	};
 };
 
 true;
