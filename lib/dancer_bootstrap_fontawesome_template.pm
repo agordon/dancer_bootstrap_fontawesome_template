050ee05426b407b9a0914a95391e1972670edc42
A. Gordon <gordon@cshl.edu>
2012-05-25 17:43:16 -0400

    Deployment stup for apache+cgi

--- lib/dancer_bootstrap_fontawesome_template.pm
+++ lib/dancer_bootstrap_fontawesome_template.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Cwd;
 use Sys::Hostname;
+use Data::Dump qw/dump/;
 
 our $VERSION = '0.1';
 
@@ -12,11 +13,29 @@ get '/' => sub {
 };
 
 get '/deploy' => sub {
-    my $project_dir = getcwd();
-    my $hostname = hostname;
+    template 'deployment_wizard', {
+		directory => getcwd(),
+		hostname  => hostname(),
+		proxy_port=> 8000,
+		cgi_type  => "fast",
+		fast_static_files => 1,
+	};
+};
+
+#The user clicked "updated", generate new Apache/lighttpd/nginx stubs
+post '/deploy' => sub {
+    my $project_dir = param('input_project_directory') || "";
+    my $hostname = param('input_hostname') || "" ;
+    my $proxy_port = param('input_proxy_port') || "";
+    my $cgi_type = param('input_cgi_type') || "fast";
+    my $fast_static_files = param('input_fast_static_files') || 0;
+
     template 'deployment_wizard', {
 		directory => $project_dir,
-		hostname  => $hostname
+		hostname  => $hostname,
+		proxy_port=> $proxy_port,
+		cgi_type  => $cgi_type,
+		fast_static_files => $fast_static_files,
 	};
 };
 
