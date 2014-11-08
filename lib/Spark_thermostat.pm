93e7cd5e91e025b837eb3e339a839207e02c5b1d
Matthew Berginski <matthew.berginski@gmail.com>
2014-11-07 20:01:04 -0500

    adding last update time value

--- lib/Spark_thermostat.pm
+++ lib/Spark_thermostat.pm
@@ -9,6 +9,7 @@ use Text::CSV::Simple;
 use Dancer::Plugin::Passphrase;
 use Data::Dumper;
 use Config::Simple;
+use DateTime::Format::ISO8601;
 
 our $VERSION = '0.1';
 
@@ -24,7 +25,7 @@ get '/' => sub {
 	my @data = $parser->read_file('../utilities/last_min.csv');
 	
 	my @last_data = @{$data[-1]};
-	
+
 	my $relay_status = 'Off';
 	$relay_status = 'On' if ($last_data[3]);
 	
@@ -33,6 +34,10 @@ get '/' => sub {
 		$relay_color = '"Green"';
 	}
 
+	my $dt = DateTime::Format::ISO8601->parse_datetime($last_data[0]);	
+	$dt->set_time_zone('UTC');
+	$dt->set_time_zone('EST');
+	
     template 'index', { 
 		'freezer_temp' => $last_data[1],
 		'outside_temp' => $last_data[2],
@@ -40,6 +45,7 @@ get '/' => sub {
 		'relay_status' => $relay_status,
 		'relay_color' => $relay_color,
 		'image_set' => \@image_set,
+		'last_time' => $dt->month() . "/" . $dt->day() ." " . $dt->hms,
 	};
 };
 
@@ -60,28 +66,33 @@ post '/' => sub {
 		$relay_color = '"Green"';
 	}
 	
+	my $dt = DateTime::Format::ISO8601->parse_datetime($last_data[0]);	
+	$dt->set_time_zone('UTC');
+	$dt->set_time_zone('EST');
+	
+	#Relay Control
 	my $cfg = new Config::Simple('../lib/password.cfg') or die "$!";
 	
-	my $warning_message = '';
+	my $settings_update_msg = '';
 	my $alert_class = '';
 	my $password_match = 0;
 
 	if (not(passphrase(params->{'password'})->matches($cfg->param('hash_pass')))) {
-		$warning_message .= 'Password doesn\'t match, no change made in settings.<br/>';
+		$settings_update_msg .= 'Password doesn\'t match, no change made in settings.<br/>';
 		$alert_class = "alert-danger";
 	} else {
-		$warning_message .= 'Success, mode updated.';
+		$settings_update_msg .= 'Success, mode updated.';
 		$alert_class = "alert-success";
 		$password_match = 1;
 	}
 	
 	# if (params->{'mode'} eq 'Constant') {
 	# 	if (params->{'targetTemp'} < 32) {
-	# 		$warning_message .= 'Can\'t set temperature below 32.<br/>';
+	# 		$settings_update_msg .= 'Can\'t set temperature below 32.<br/>';
 	# 	}
 	# } elsif (params->{'mode'} eq 'Ramp') {
 	# 	if (params->{'rampTime'} < 0.5) {
-	# 		$warning_message .= 'Can\'t set temperature below 32.<br/>';
+	# 		$settings_update_msg .= 'Can\'t set temperature below 32.<br/>';
 	# 	}
 	# }
 	
@@ -105,8 +116,9 @@ post '/' => sub {
 		'relay_status' => $relay_status,
 		'relay_color' => $relay_color,
 		'image_set' => \@image_set,
-		'alert_message' => $warning_message,
+		'alert_message' => $settings_update_msg,
 		'alert_class' => $alert_class,
+		'last_time' => $dt->month() . "/" . $dt->day() ." " . $dt->hms,
 	};
 };
 
