e5d55dd9c08456d0e5d1bd848e81f3060c057c65
Matthew Berginski <matthew.berginski@gmail.com>
2016-02-14 20:10:11 +0000

    mostly permission changes, with new crontab

--- utilities/buildAndInstallCron.pl
+++ utilities/buildAndInstallCron.pl
@@ -6,8 +6,7 @@ use Cwd;
 my $dir = getcwd;
 
 open OUTPUT, ">temp_cron" or die $!;
-print OUTPUT "* * * * * cd $dir; ./record_temperature.pl\n";
-print OUTPUT "* * * * * cd $dir; ./plot_temperatures.R temperature_data.csv ../public/images/\n";
+print OUTPUT "* * * * * cd $dir; ./record_temperature.pl; ./plot_temperatures.R temperature_data.csv ../public/images/\n";
 close OUTPUT;
 
 system "crontab temp_cron";
